01c51cb97ac0860e8d744cb0e9fe4d741a29d7af
Egor Soldatov <egorka.sold@gmail.com>
2021-08-21 03:42:41 +0700

    row

--- bot_creator.py
+++ bot_creator.py
@@ -3,6 +3,7 @@ from aiogram.dispatcher import Dispatcher
 from aiogram import executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from database import read
+from asyncio import sleep
 
 import states
 import keyboards
@@ -27,6 +28,35 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
+@dp.callback_query_handler(lambda call: call.data.startswith('getInfo_'))
+async def da(call):
+    admin = call['from']['id']
+    shop = call.data[8:]
+    kb = types.InlineKeyboardMarkup()
+    kb.add(types.InlineKeyboardButton(callback_data=f'myShops_{admin}', text='Назад'))
+    await bot.edit_message_text(
+        chat_id=call.message.chat.id,
+        text=f"Магазин {shop}",
+        message_id=call.message.message_id,
+        reply_markup=kb
+    )
+
+
+@dp.callback_query_handler(lambda call: call.data.startswith('myShops_'))
+async def da(call):
+    admin = call.data[8:]
+    rez = read("SELECT * FROM shops;")
+    rez = [i[1] for i in rez if str(admin) in i[3].split()]
+    kb = types.InlineKeyboardMarkup()
+    for i in rez:
+        kb.add(types.InlineKeyboardButton(text=i, callback_data="getInfo_" + i))
+    await bot.edit_message_text(
+        chat_id=call.message.chat.id,
+        text=f"Мои магазины:",
+        message_id=call.message.message_id,
+        reply_markup=kb
+    )
+
 
 async def username2id(username):
     async with TelegramClient('hhsked', 7891326, "b626fc3516cdef753a9d27dcf096fd25") as client:
@@ -42,7 +72,7 @@ async def is_bot(username):
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message):
-    await bot.send_message(message.from_user.id, MESSAGES['start'], reply_markup=keyboards.bot_creator_keyboard, )
+    await bot.send_message(message.from_user.id, MESSAGES['start'], reply_markup=keyboards.bot_creator_keyboard)
 
 
 @dp.message_handler(commands=['help'])
@@ -59,20 +89,25 @@ async def process_set_admin_command(message):
         await bot.send_message(message.from_user.id, 'Invalid Syntax', reply_markup=keyboards.bot_creator_keyboard)
 
 
+@dp.message_handler(
+    lambda message: [message.text] in keyboards.bot_creator_keyboard['keyboard'] and message.text == 'Мои магазины')
+async def process_three_base_commands(message):
+    admin = message.from_user.id
+    rez = read("SELECT * FROM shops;")
+    rez = [i[1] for i in rez if str(admin) in i[3].split()]
+    kb = types.InlineKeyboardMarkup()
+    for i in rez:
+        kb.add(types.InlineKeyboardButton(text=i, callback_data="getInfo_" + i))
+    await message.answer("Мои магазины:", reply_markup=kb)
+
+
 @dp.message_handler(lambda message: [message.text] in keyboards.bot_creator_keyboard['keyboard'])
 async def process_three_base_commands(message):
     if message.text == 'Добавить магазин':
         await message.answer(MESSAGES['Добавить магазин'])
         await states.TwoStates.ADD_SHOP.set()
-
-    elif message.text == 'Мои магазины':
-        admin = message.from_user.id
-        rez = read("SELECT * FROM shops;")
-        rez = [i[1] for i in rez if str(admin) in i[3].split()]
-        kb = types.InlineKeyboardMarkup()
-        for i in rez:
-            kb.add(types.InlineKeyboardButton(text=i, callback_data="getInfo_" + i))
-        await message.answer("Мои магазины:", reply_markup=kb)
+    else:
+        await message.answer(MESSAGES[message.text])
 
 
 @dp.message_handler(state=states.TwoStates.ADD_SHOP)
@@ -86,8 +121,9 @@ async def process_add_shop_command(message):
         await states.TwoStates.next()
     else:
         await bot.send_message(message.from_user.id, "Токен невалиден")
-
-
+        await states.TwoStates.next()
+        await sleep(0.7)
+        await bot.send_message(message.from_user.id, "Вы в меню")
 
 
 if __name__ == '__main__':
--- keyboards.py
+++ keyboards.py
@@ -1,7 +1,10 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram import types
+from aiogram.types import ReplyKeyboardRemove, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 
+inline_btn_1 = InlineKeyboardButton('Первая кнопка!', callback_data='button1')
+inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1)
 
-bot_creator_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+bot_creator_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 bot_creator_keyboard.add('Мои магазины')
 bot_creator_keyboard.add('Добавить магазин')
 bot_creator_keyboard.add('Связаться с оператором')
