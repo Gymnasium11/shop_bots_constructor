02a8326a1ca2782ed2140dd17c58e753a9c4c283
Alexander Mangazeev <alex3287@bk.ru>
2021-08-20 20:38:54 +0700

    fix database

--- database.py
+++ database.py
@@ -6,20 +6,21 @@ def read(request):
     try:
         sqlite_connection= sqlite3.connect('base.db', timeout=20)
         cursor = sqlite_connection.cursor()
-        print("Подключен к SQLite")
+        # print("Подключен к SQLite")
 
         sqlite_select_query = request
         cursor.execute(sqlite_select_query)
-        total_rows = cursor.fetchone()
-        print("Всего строк:  ", total_rows)
+        total_rows = cursor.fetchall()
+        # print("Всего строк:  ", total_rows)
         cursor.close()
+        return total_rows
 
     except sqlite3.Error as error:
         print("Ошибка при подключении к sqlite", error)
     finally:
         if (sqlite_connection):
             sqlite_connection.close()
-            print("Соединение с SQLite закрыто")
+            # print("Соединение с SQLite закрыто")
 
 
 def insert(reaquest):
@@ -27,13 +28,13 @@ def insert(reaquest):
     try:
         sqlite_connection = sqlite3.connect('base.db')
         cursor = sqlite_connection.cursor()
-        print("База данных подключена к SQLite")
+        # print("База данных подключена к SQLite")
 
         sqlite_insert_query = reaquest
 
         count = cursor.execute(sqlite_insert_query)
         sqlite_connection.commit()
-        print("Запись успешно вставлена ​​в таблицу sqlitedb_developers ", cursor.rowcount)
+        # print("Запись успешно вставлена ​​в таблицу sqlitedb_developers ", cursor.rowcount)
         cursor.close()
 
     except sqlite3.Error as error:
@@ -52,4 +53,4 @@ def insert(reaquest):
 if __name__ == "__main__":
     # insert("""INSERT INTO shop
     #                           (name, token, admins, rating, count_solled, discreption)  VALUES  ("third", "this_is_token_1111", "admin3", 4.1, 1520, "third shop for test")""")
-    read("SELECT * FROM shop")
+    print(read("SELECT * FROM shop"))
