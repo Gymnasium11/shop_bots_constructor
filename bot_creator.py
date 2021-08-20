f5e3e75c44c037c320f24f4287c97359dd6e8195
Egor Soldatov <egorka.sold@gmail.com>
2021-08-20 19:19:49 +0700

    Yet another changes in bot_creator

--- bot_creator.py
+++ bot_creator.py
@@ -1,7 +1,6 @@
-from aiogram import Bot, types
+from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 from aiogram import executor
-from aiogram.dispatcher.filters import BoundFilter
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 import states
@@ -22,26 +21,21 @@ async def process_start_command(message):
 async def process_start_command(message):
     await bot.send_message(message.from_user.id, MESSAGES['help'], reply_markup=keyboards.bot_creator_keyboard)
 
+@dp.message_handler(commands=['set_admin'])
+async def process_set_admin_command(message):
+    await bot.send_message(message.from_user.id, message)
+
 @dp.message_handler(lambda message: [message.text] in keyboards.bot_creator_keyboard['keyboard'])
 async def process_three_base_commands(message):
     await bot.send_message(message.from_user.id, MESSAGES[message.text])
     if message.text == 'Добавить магазин':
         await states.TwoStates.ADD_SHOP.set()
-    elif message.text == 'Мои магазины':
-        await states.TwoStates.ADD_SHOP.set()
 
-@dp.message_handler(state=states.ThreeStates.ADD_SHOP)
+@dp.message_handler(state=states.TwoStates.ADD_SHOP)
 async def process_add_shop_command(message):
     token = message.text
     await bot.send_message(message.from_user.id, MESSAGES['done'])
     await states.TwoStates.next()
-
-
-@dp.message_handler(state=states.TwoStates.MY_SHOPS)
-async def process_my_shops_command(message):
-    await bot.send_message(message.from_user.id, MESSAGES['done'])
-    await states.TwoStates.next()
-
 
 if __name__ == '__main__':
     executor.start_polling(dp)
