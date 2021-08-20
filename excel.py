d6d11beac082480d7e0aa145c8ffbc68f6e8e962
Alexander Mangazeev <alex3287@bk.ru>
2021-08-20 22:02:35 +0700

    version 0.4

--- excel.py
+++ excel.py
@@ -1,10 +1,17 @@
 import pandas as pd
+import database
 
-excel_data_df = pd.read_excel('./files/1/catalog2.xlsx', sheet_name='Sheet1')
+def read_excel(path):
+    excel_data_df = pd.read_excel(path)
+    # excel_data_df = excel_data_df.replace(pd.NA, 'null')
+    for i in range(len(excel_data_df)):
+        line = list(excel_data_df.iloc[i])
+        print(line, line[1].strip())
+        id_shop = int(path.split('/')[-2])
+        database.insert(f"INSERT INTO product (id_shop, article, name, price, category, short_description, description, url_picture, color )  VALUES  ('{id_shop}', '{line[0]}', '{line[1]}', '{line[2]}', '{line[3]}','{line[4]}','{line[5]}','{line[6]}', '{line[7]}')")
 
-# print whole sheet data
-# print(excel_data_df)
 
-# print(excel_data_df.columns.ravel())
-for i in range(len(excel_data_df)):
-    print(list(excel_data_df.iloc[i]))
+read_excel('./files/1/catalog2.xlsx')
+
+# price, category, short_description, description, url_picture
+# , {line[2]}, {line[3]},{line[4]},{line[5]},{line[6]}
